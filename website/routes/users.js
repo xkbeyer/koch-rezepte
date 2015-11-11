661d9c5177e55e378428caad60eb57e36fe04bf6
xkbeyer <xkbeyer@gmail.com>
2015-11-11 20:11:04 +0100

    Changed db user. Adapt query to changed table.

--- website/routes/users.js
+++ website/routes/users.js
@@ -4,22 +4,22 @@ var router = express.Router();
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : 'klaus',
+    user     : 'kochmate',
+    password : 'epsilon-signum',
     database : 'koch-rezepte'
 });
 connection.connect();
 
 
 function getData(res) {
-    connection.query('SELECT * from rezept', function (err, rows, fields) {
+    connection.query('SELECT id, name, description from rezept', function (err, rows, fields) {
         if (err) {
             console.log(err);
             throw err;
         }
         var data = [];
         for( var i in rows) {
-            data.push( { id : rows[i].id, desc : rows[i].description } );
+            data.push( { id : rows[i].id, name : rows[i].name, desc : rows[i].description } );
         }
         var complData = { title: "Content", list: data };
         res.render('dbonerecord', complData );
